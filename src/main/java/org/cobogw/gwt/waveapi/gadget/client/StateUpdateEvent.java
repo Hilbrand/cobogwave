a2a41d1497b13298680c9ffa0845ab0bb0ed8cf3
Hilbrand Bouwkamp <hilbrand@h72.nl>
2009-10-03 10:27:52 +0000

    Added method log to WaveFeature.

--- src/main/java/org/cobogw/gwt/waveapi/gadget/client/StateUpdateEvent.java
+++ src/main/java/org/cobogw/gwt/waveapi/gadget/client/StateUpdateEvent.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2009 Hilbrand Bouwkamp, hs@bouwkamp.com
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
  * the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
  * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
@@ -19,7 +19,7 @@ import com.google.gwt.event.shared.GwtEvent;
 import com.google.gwt.event.shared.HasHandlers;
 
 public class StateUpdateEvent extends GwtEvent<StateUpdateEventHandler> {
-  
+
   /**
    * Handler type.
    */
@@ -28,7 +28,7 @@ public class StateUpdateEvent extends GwtEvent<StateUpdateEventHandler> {
 
   /**
    * Gets the type associated with this event.
-   * 
+   *
    * @return returns the handler type
    */
   public static Type<StateUpdateEventHandler> getType() {
@@ -60,7 +60,7 @@ public class StateUpdateEvent extends GwtEvent<StateUpdateEventHandler> {
   public Type<StateUpdateEventHandler> getAssociatedType() {
     return (Type) TYPE;
   }
-  
+
   public State getState() {
     return wave.getState();
   }
