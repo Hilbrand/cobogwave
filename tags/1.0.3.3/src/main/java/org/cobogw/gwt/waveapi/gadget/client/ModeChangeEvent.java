a67ae5a1c6b535fcacba09bdf708fe617a98cbd3
hs@bouwkamp.com <hs@bouwkamp.com@fb668eac-8142-11de-a005-139651f65d8c>
2009-11-09 10:11:30 +0000

    Tagged release 1.0.3.3

---
+++ tags/1.0.3.3/src/main/java/org/cobogw/gwt/waveapi/gadget/client/ModeChangeEvent.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2009 Hilbrand Bouwkamp, hs@bouwkamp.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package org.cobogw.gwt.waveapi.gadget.client;
+
+import com.google.gwt.event.shared.GwtEvent;
+import com.google.gwt.event.shared.HasHandlers;
+
+/**
+ * Event fired when the wave mode changes. 
+ */
+public class ModeChangeEvent extends GwtEvent<ModeChangeEventHandler> {
+
+  /**
+   * Handler type.
+   */
+  private static Type<ModeChangeEventHandler> TYPE;
+
+  /**
+   * Gets the type associated with this event.
+   *
+   * @return returns the handler type
+   */
+  public static Type<ModeChangeEventHandler> getType() {
+    if (TYPE == null) {
+      TYPE = new Type<ModeChangeEventHandler>();
+    }
+    return TYPE;
+  }
+
+  static ModeChangeEvent fire(HasHandlers source, int mode) {
+    // If no handlers exist, then type can be null.
+    if (TYPE != null) {
+      final ModeChangeEvent event = new ModeChangeEvent(mode);
+      source.fireEvent(event);
+      return event;
+    }
+    return null;
+  }
+
+  private int mode;
+
+  private ModeChangeEvent(int mode) {
+    this.mode = mode;
+  }
+
+  protected void dispatch(ModeChangeEventHandler handler) {
+    handler.onUpdate(this);
+  }
+
+  @SuppressWarnings("unchecked")
+  public Type<ModeChangeEventHandler> getAssociatedType() {
+    return (Type) TYPE;
+  }
+
+  public int getMode() {
+    return mode;
+  }
+}
